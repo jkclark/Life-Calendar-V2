fb7c7c1f8bd59d2572ec688eaf8d94b9c3fd3d8d
Josh Clark <jclarktennis@gmail.com>
2025-10-31 15:48:59 -0400

    feat: add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="text-primary">Vite + React</div>;
+  return (
+    <div className="flex flex-col">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
