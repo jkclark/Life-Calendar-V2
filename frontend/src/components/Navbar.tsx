db82644fb284d3b6f00145fd744aac1e9c1ed062
Josh Clark <jclarktennis@gmail.com>
2025-11-01 11:54:30 -0400

    chore: remove shadow from navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="navbar bg-base-100 shadow-md select-none">
+    <div className="navbar bg-base-100 select-none">
       <div className="navbar-start">
         <div className="px-4 py-3 text-xl font-semibold">Life Calendar</div>
       </div>
